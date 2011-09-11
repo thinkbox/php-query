b4f32af92093447983eef5e2298e678c2ea166e7
pariskasidiaris <pariskasidiaris@6f3f2ad3-3130-2f1e-e7e6-24cdb9690071>
2011-09-11 17:59:21 +0000

    Added 'run' public function.

--- phpquery.php
+++ phpquery.php
@@ -47,5 +47,51 @@ public function prepare_mysql_data ( $data , $return = 'dynamic' ) {
 
 }
 
+public function run ( $query , $data , $return = 'dynamic' ) {
+
+	$data = $this -> prepare_mysql_data ( $data , 'array' );
+
+	if ( $data [ 'successful' ] === false ) {
+		return $this -> return_data ( $data , $return );
+	}
+
+	$parts = explode( '?' , $query );
+
+	$query = '';
+
+	foreach ( $data as $value ) {
+
+		$query .= array_shift( $parts );
+
+		$query .= $value;
+
+	}
+	
+	$query .= array_shift( $parts );
+
+	$res = mysql_query ( $query );
+
+	$r = array ();
+
+	if ( is_resource ( $res ) ) {
+
+		$r [ 'successful' ] = true;
+		$r [ 'data' ] = $res;
+
+	}else {
+
+		$r [ 'successful' ] = $res;
+
+		if ( $res === false ) {
+			$r [ 'error' ] = mysql_error ();
+		}
+
+	}
+
+	return $this -> return_data ( $r , $return );
+
+	return mysql_query( $query ) or die( mysql_error() );
+}
+
 }
 ?>
